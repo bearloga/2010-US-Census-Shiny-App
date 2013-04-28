bbee4ee8ceea3781417b924261325b926c2be6f9
Mikhail Popov <mikhail@Mikhails-Mac-mini.local>
2013-04-28 19:39:51 -0400

    Defaults to border

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -180,7 +180,7 @@ shinyServer(function(input, output) {
 			
 			# If the user is colorblind and a gray scale is selected,
 			# then borders are added to the grayscale polygons.
-			border=ifelse(chosenColor()=='gray',"black","transparent")
+			border="black")#,"transparent")
 			
 		)
 		# IF the user has specified that she/he wants to use custom labels,
